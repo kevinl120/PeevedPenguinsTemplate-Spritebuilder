60d980c1f0510c0933266fcca83da6beba8cc7dd
Kevin Li <kevinli@Kevins-MacBook-Pro.local>
2014-06-27 11:11:23 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -158,6 +158,15 @@
 }
 
 
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
+
+
 - (void) sealRemoved:(CCNode *)seal {
     // Load particle effect
     CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
