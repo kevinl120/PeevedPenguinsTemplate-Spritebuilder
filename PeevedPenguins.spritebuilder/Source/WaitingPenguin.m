0e3fda1b9257bec9103d169bb7e306418749bcca
Kevin Li <kevinli@Kevins-MacBook-Pro.local>
2014-06-27 11:31:40 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -10,13 +10,18 @@
 
 @implementation WaitingPenguin
 
-- (void)didLoadFromCCB {
-    self.physicsBody.collisionType = @"seal";
+- (void) didLoadFromCCB {
+   
+    // Generate a random number between 0.0 and 2.0
+    float delay = (arc4random() %2000) / 1000.f;
+    // Call method to start animation after random delay
+    [self performSelector:@selector(startBlinkAndJump) withObject: nil afterDelay: delay];
+   
 }
 
 
-- (void)startBlinkAndJump
-{
+- (void)startBlinkAndJump {
+    
     // the animation manager of each node is stored in the 'animationManager' property
     CCAnimationManager* animationManager = self.animationManager;
     // timelines can be referenced and run by name
