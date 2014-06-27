2a314eec3b04d1fd28499e332ab52e80329c8df5
Kevin Li <kevinli@Kevins-MacBook-Pro.local>
2014-06-27 11:18:26 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -174,4 +174,11 @@
 }
 
 
+- (void) startBlinkAndJump {
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
+
 @end
